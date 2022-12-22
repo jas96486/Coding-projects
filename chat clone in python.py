d3b250e86ba891fc1b77a3cce499f5a771b904be
Jaskaran Soni <104744741+jas96486@users.noreply.github.com>
2022-12-21 20:47:10 -0500

    Add files via upload

---
+++ chat clone in python.py
@@ -0,0 +1,55 @@
+import socket
+import threading
+
+HOST = '10.0.0.84'
+PORT = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(HOST, PORT)
+
+server.listen()
+
+clients = []
+nicknames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f'{nicknames[clients.index(client)]} says {message}')
+            broadcast(message)
+        except:
+            index = clients.index(clients)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'Connect with {str(address)}')
+
+
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of the client is {nickname}')
+
+        broadcast(f'{nickname} connected to the server!\n'.encode('utf-8'))
+        client.send('connect to the server'.encode('utf-8'))
+
+        thread= threading.Thread(target=handle,args=(client))
+
+print('server runing')
+receive()
